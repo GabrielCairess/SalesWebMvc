93c3d3f453a82902f2497050d649c8828ed07073
gabrielcairess <gbrlcaires@gmail.com>
2020-04-20 17:50:51 -0300

    adding other classes and second migration

---
+++ Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; private set; }
+        public ICollection<SalesRecord> SalesRecords { get; private set; } = new List<SalesRecord>();
+        public Department Department { get; private set; }
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSale(SalesRecord sr)
+        {
+            SalesRecords.Add(sr);
+        }
+
+        public void RemoveSale(SalesRecord sr)
+        {
+            SalesRecords.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return SalesRecords.Where(s => s.Date >= initial && s.Date <= final).Sum(s => s.Amount);
+        }
+    }
+}
